9ba8568f38757341892e60f247038f8afca7cd52
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-12-14 23:39:18 +0300

    Changes to be committed: 	modified:   .env 	new file:   config/db.js 	modified:   index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./config/db');
 
 const app = express();
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('❌ MySQL connection failed:', err.message);
+  } else {
+    console.log('✅ MySQL connected successfully');
+    connection.release();
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Agri Clinic Hub API running 🟢');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
